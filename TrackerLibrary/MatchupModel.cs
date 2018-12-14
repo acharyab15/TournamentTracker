e53048534a6901b0a5d9c10fca03e593553a44da
acharyab15 <acharyab@seas.upenn.edu>
2018-12-13 19:25:35 -0800

    Initial commit

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntriesModel> Entries { get; set; } = new List<MatchupEntriesModel>();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
