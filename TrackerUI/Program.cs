c87d5876ffa87c371e5b2d48cb2c84a05b5d64b7
acharyab15 <acharyab@seas.upenn.edu>
2019-04-19 08:49:11 -0700

    TextConnector: add a way to write to a text file.

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -19,7 +19,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize the database connections
-            GlobalConfig.InitializeConnection(DatabaseType.Sql);
+            GlobalConfig.InitializeConnection(DatabaseType.TextFile);
             Application.Run(new CreatePrizeForm());
 
             // Application.Run(new TournamentDashBoardForm());
